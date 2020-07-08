a5289e2eaf2528d44058ff19e4c398dfa6551b86
Drilbur_CST126 <jordan.clark@oit.edu>
2020-07-07 20:40:10 -0700

    Initial commit, basic visuals

---
+++ app/src/main/java/com/jnich/homework2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jnich.homework2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
